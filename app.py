d64d249be0d9d69c3e394675c50ce6e9ba9de15c
RUBERVALALVES <30862844+RUBERVALALVES@users.noreply.github.com>
2026-08-20 14:26:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from tensorflow.keras.preprocessing import image
 app = Flask(__name__)
 
 # Carrega o modelo salvo previamente (ex: formato .keras ou .h5)
-MODEL_PATH = '//SUPERFRANGO/modelo_frango.h5'
+MODEL_PATH = '/SUPERFRANGO/modelo_frango.h5'
 
 model = load_model(MODEL_PATH)
 
