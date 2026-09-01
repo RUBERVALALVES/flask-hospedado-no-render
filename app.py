0b6716913e4d2da72b051c2c2334a4dc6f55c504
RUBERVALALVES <30862844+RUBERVALALVES@users.noreply.github.com>
2026-09-01 16:38:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import io
 app = Flask(__name__)
 
 # Carrega o modelo de aprendizado de máquina
-model = tf.keras.models.load_model('modelo_frango.h5')
+model = tf.keras.models.load_model('modelo_otimizado.tflite')
 
 
 # Ajuste as dimensões de acordo com o seu modelo
