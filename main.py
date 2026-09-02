d3cf521a6be323ea1ab93c4e8ec5b7a8dd4bd87e
RUBERVALALVES <30862844+RUBERVALALVES@users.noreply.github.com>
2026-09-02 15:37:46 -0300

    Change confidence threshold from 0.5 to 50

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def getPrediction(filename):
     #predicted_index = int(np.argmax(output_data))
     #confidence = float(output_data[predicted_index])
 
-    if confidence < 0.5:  
+    if confidence < 50:  
         return "Invalid"
     return classes[predicted_index]
 
