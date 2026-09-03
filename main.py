4d37c1d98d2ff51887870018417a9e6535c77405
RUBERVALALVES <30862844+RUBERVALALVES@users.noreply.github.com>
2026-09-03 09:23:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -67,5 +67,5 @@ def getPrediction(filename):
 
     if confidence < 0.5:  
         return "Imagem Invalida"
-    return classes[predicted_index],  confidence
+    return classes[predicted_index],  confidence, probabilities
 
