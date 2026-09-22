24a479dc3e812a26eb0dfdb31c1b5e471f6ac446
RUBERVALALVES <30862844+RUBERVALALVES@users.noreply.github.com>
2026-09-22 08:23:20 -0300

    Refactor app.py to improve structure and clarity

--- app.py
+++ app.py
@@ -114,4 +114,6 @@ def predict():
         return jsonify({'erro': str(e)}), 500
 
 
-
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
