064cac2721ca504da106f62ed8b262a6b12e5a3c
RUBERVALALVES <30862844+RUBERVALALVES@users.noreply.github.com>
2026-09-24 16:27:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -57,11 +57,9 @@ def getPrediction(filename):
  
    
     #apenas_digitos = [f"{x:.3f}" for x in probabilities]
+    apenas_digitos = [f"{x:.2e}".split('e')[0] for x in probabilities]
     
-    #Converte o número original em string e pega os 4 primeiros caracteres (ex: "0.99" ou "9.98")
-    apenas_digitos = [str(x)[:4] for x in probabilities]
-    # Margem de diferença entre a 1ª e a 2ª maior probabilidade
-    #margin = top1_prob + top2_prob + top3_prob + top4_prob
+   
 
     # Calcula a entropia normalizada
     entropy = -np.sum(probabilities * np.log(probabilities + 1e-10))
