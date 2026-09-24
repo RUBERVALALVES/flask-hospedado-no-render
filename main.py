53a4caf4a6ca8d70f9a62a741026a71e50830b7c
RUBERVALALVES <30862844+RUBERVALALVES@users.noreply.github.com>
2026-09-24 15:00:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def getPrediction(filename):
     top3_prob = sorted_probs[2] 
     top4_prob = sorted_probs[3] 
 
-    apenas_digitos = f"{sorted_probs[0]:.10f}"[:3]
+    apenas_digitos = f"{probabilities[0]:.10f}"[:3]
 
     
 
