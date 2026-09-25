d62e32137f52404d03138ae03d4564a14f183ab4
RUBERVALALVES <30862844+RUBERVALALVES@users.noreply.github.com>
2026-09-25 10:51:20 -0300

    Update logic to format probabilities based on value
    Refactor number formatting logic for probabilities.

--- main.py
+++ main.py
@@ -64,7 +64,11 @@ def getPrediction(filename):
 
     # Se for um valor decimal comum menor que 0.1 (ex: 0.0445), retorna "0.0"
     # Caso contrário, extrai os 3 primeiros caracteres da mantissa (ex: "9.9")
-    apenas_digitos = [f"{x:.8e}".split("e")[0][:3] if x >= 0.1 else "0.0" for x in probabilities]
+    # Se for menor que 1 (ex: 0.9004522), pega o formato decimal comum "0.9"
+    # Se for 1 ou mais (ou notação com mantissa >= 1), pega os 3 primeiros caracteres da notação científica "9.9"
+    apenas_digitos = [
+    f"{x:.8e}".split("e")[0][:3] if "e" in str(x) else str(x)[:3]
+    for x in probabilities]
     #apenas_digitos = [f"{x:.8e}".split("e")[0][:4] for x in probabilities]
     #apenas_digitos2 = ["0" if float(x) < 1 else x for x in apenas_digitos]
 
